1225207418a4c9678da7cd8fa63134b12afbdad1
Ezekiel <ezekiel@itsmart.my>
2024-11-21 22:47:42 +0800

    updated users column to use text instead

--- internal/database/migrations/000002_users.up.sql
+++ internal/database/migrations/000002_users.up.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS users (
     id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
     email varchar(255) NOT NULL,
-    user_type varchar(255) NOT NULL,
+    user_type text NOT NULL,
     password_hash text,
     created_at timestamp DEFAULT now(),
     updated_at timestamp 
